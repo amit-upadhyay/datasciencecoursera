02ec42fec8750bd1c2461cc5a27790892ae989d7
amit-upadhyay <amitupadhyay86@gmail.com>
2024-01-12 20:33:46 -0500

    Modify NA removal

--- Quiz1.R
+++ Quiz1.R
@@ -15,11 +15,11 @@ mean(data$Ozone[!is.na(data$Ozone)])
 
 
 sub <- data[data$Ozone > 31 & data$Temp >90,]
-sub <- sub[complete.cases(sub),]
-mean(sub$Solar.R)
+# sub <- sub[complete.cases(sub),]
+mean(sub$Solar.R, na.rm = T)
 
 mean(data[data$Month == 6,]$Temp)
 
-max(data[data$Month == 5 & !is.na(data$Ozone),]$Ozone)
+max(data[data$Month == 5,]$Ozone,na.rm=T)
 
 
